1050ddf751abea07414c2970b1068b1e2dd1aa78
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-05-05 17:52:41 +0200

    Mise en place du controller Default

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index()
+    {
+        return new Response(
+            '<html><body>Blog Index</body></html>'
+        );
+    }
+}
