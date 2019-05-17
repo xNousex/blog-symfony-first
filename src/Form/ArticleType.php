912fc118e32bba5cda1a0488d79378e4e04c1274
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-05-17 19:03:46 +0200

    Mise en forme du formulaire ajout/modif/delete

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -12,9 +12,23 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('category', null, ['choice_label' => 'name'])
+            ->add('title', null, [
+                'label' => 'Titre :',
+                'attr' => array('class' => 'form-control w-100')
+            ])
+            ->add('content', null, [
+                'label' => 'Description :',
+                'attr' => [
+                    'class' => 'form-control w-100',
+                    'rows' => '8']
+            ])
+            ->add('category', null, [
+                'choice_label' => 'name',
+                'label' => 'Type d\'article :',
+                'attr' => [
+                    'class' => 'form-control w-100'
+                ]
+            ])
         ;
     }
 
