a91c776cba7049c1789dc52037a5c00c18f4f36d
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-05-18 12:20:29 +0200

    Finalisation de la page de listing des tags

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TagController extends AbstractController
 {
     /**
+     * @var TagRepository
+     */
+    private $repository;
+
+    public function __construct(TagRepository $repository)
+    {
+
+        $this->repository = $repository;
+    }
+
+    /**
      * @Route("/tag", name="tag")
      */
     public function index()
     {
+        $tags = $this->repository->findAll();
         return $this->render('tag/index.html.twig', [
             'controller_name' => 'TagController',
+            'tags' => $tags,
         ]);
     }
 }
