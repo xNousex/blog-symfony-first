1e16cb8cd6b5038f8cbd823560f4428a9010535f
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-06-02 17:36:39 +0200

    Ajout de contraintes sur titre (max:255) et titre unique

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ * @UniqueEntity("title", message="Ce titre existe déjà")
  */
 class Article
 {
@@ -20,6 +23,9 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Ce champ ne peut pas être vide")
+     * @Assert\Length(max="255",maxMessage="La catégorie saisie {{ value }} est trop longue, {{ limit }} caractères maximum")
+     *
      */
     private $title;
 
